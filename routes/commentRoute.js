c8e414e78feb9e1ee95aa603966f28f2e4b76734
kaushik.bora <herock76@gmail.com>
2024-03-19 16:56:00 +0530

    -comment route and controller complete

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,7 +1,15 @@
 import express from 'express';
-import {} from '../controllers/commentController.js';
+import {
+  addComment,
+  deleteComment,
+  getComments,
+} from '../controllers/commentController.js';
+import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router();
-router.get('/test');
+
+router.post('/', verifyToken, addComment);
+router.delete('/:id', verifyToken, deleteComment);
+router.get('/:videoId', getComments);
 
 export default router;
