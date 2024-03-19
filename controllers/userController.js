8043e768df42154c22989e0856379b7aad3bd161
kaushik.bora <herock76@gmail.com>
2024-03-19 18:15:02 +0530

    -added l;ike and dislike controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 import { createError } from '../utils/error.js';
 import User from '../models/User.js';
+import Video from '../models/Video.js';
 
 export const update = async (req, res, next) => {
   if (req.params.id === req.user.id) {
@@ -94,14 +95,32 @@ export const unsubscribe = async (req, res, next) => {
 };
 
 export const like = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
+  console.log(id, videoId);
+
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { likes: id },
+      $pull: { dislikes: id },
+    });
+
+    res.status(200).json('Video has been liked');
   } catch (error) {
     next(error);
   }
 };
 
 export const dislike = async (req, res, next) => {
+  const id = req.user.id;
+  const videoId = req.params.videoId;
   try {
+    await Video.findByIdAndUpdate(videoId, {
+      $addToSet: { dislikes: id },
+      $pull: { likes: id },
+    });
+
+    res.status(200).json('Video has been disliked');
   } catch (error) {
     next(error);
   }
